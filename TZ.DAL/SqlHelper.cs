0eeca6985557c0ae493799a859e4d7aefae25caa
HGJMWT <2396793225@qq.com>
2020-09-21 20:10:10 +0800

    SqlHelper

---
+++ TZ.DAL/SqlHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TZ.DAL
+{
+    public class SqlHelper
+    {
+        private static readonly string constr = ConfigurationManager.ConnectionStrings["Dbcon"].ConnectionString;
+        public static int ExecuteNonQuery(string sql, SqlParameter[] param)
+        {
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    con.Open();
+                    if (param != null) 
+                        cmd.Parameters.AddRange(param);
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public static DataTable Query(string sql, SqlParameter[] param)
+        {
+            using (SqlDataAdapter sda = new SqlDataAdapter(sql, constr))
+            {
+                if (param != null)
+                    sda.SelectCommand.Parameters.AddRange(param);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                return dt;
+            }
+        }
+
+    }
+}
